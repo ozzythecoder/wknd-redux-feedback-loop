f95138968bee11eb9685b04758e036bad7ef6a94
August McAllister <august.chm@gmail.com>
2022-12-02 14:31:30 -0600

    Set up redux & component folders

--- src/index.js
+++ src/index.js
@@ -4,5 +4,27 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const ratingsPlaceholder = {
+  feelings: 0,
+  support: 0,
+  understanding: 0,
+  comments: ''
+}
+
+const ratingsObj = (state = {}, action) => {
+  return state;
+}
+
+const reduxStore = createStore(
+  combineReducers({
+
+  }),
+  applyMiddleware(logger)
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
