cd0261471b0ef82c41b6ea38ab30b2333a8bb5f6
August McAllister <august.chm@gmail.com>
2022-12-02 15:21:59 -0600

    Added dispatch for comments to be added to the global state

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,16 +1,33 @@
-
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 export default function Comments() {
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [ commentTxtIn, setCommentTxt ] = useState(useSelector(store => store.ratingsObj.comments))
 
   const handleNext = (e) => {
     e.preventDefault()
     console.log('in handleNext');
+
+    dispatch({
+      type: 'UPDATE_RATING',
+      payload: { id: 'comments', rating: commentTxtIn }
+    })
+
   }
 
   return (
     <div>
       <h2>Anything else you'd like to add?</h2>
-      <input type='text' />
+      <input
+        type='text'
+        value={commentTxtIn}
+        onChange={e => setCommentTxt(e.target.value)}
+      />
       <button className="next-btn" onClick={handleNext}>REVIEW</button>
     </div>
   )
