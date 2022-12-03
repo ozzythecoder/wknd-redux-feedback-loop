947216fe31a1640aad5b0c1f48822f893b04a19f
August McAllister <august.chm@gmail.com>
2022-12-03 11:19:52 -0600

    Added landing screen to home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
+import { useHistory } from "react-router-dom"
 
 export default function Home() {
 
+  const history = useHistory();
+
+  const beginReview = () => {
+    history.push('/feelings')
+  }
+
   return(
     <div>
-      
+      <h2>Welcome student!</h2>
+      <h3>Let's get started on your feedback.</h3>
+      <button onClick={beginReview}>BEGIN</button>
     </div>
   )
 }
