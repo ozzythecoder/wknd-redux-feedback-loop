8a38f46520c9a94a32a13225273befd12b7b06ae
August McAllister <august.chm@gmail.com>
2022-12-03 11:59:26 -0600

    Added & imported Admin component

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default function Admin() {
+
+  
+
+  return (
+    <div>
+      <p>Admin stuff</p>
+    </div>
+  )
+}
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import { HashRouter as Router, Route } from 'react-router-dom'
 
+import Home from '../Home/Home';
 import Header from '../Header/Header';
 import RatingFeedback from '../RatingFeedback/RatingFeedback';
 import Comments from '../Comments/Comments';
 import Review from '../Review/Review';
+import Admin from '../Admin/Admin';
 import './App.css';
-import Home from '../Home/Home';
 
 function App() {
 
@@ -38,6 +39,10 @@ function App() {
 
         <Route exact path='/review'>
           <Review />
+        </Route>
+
+        <Route exact path ='/admin'>
+          <Admin />
         </Route>
         
       </Router>
