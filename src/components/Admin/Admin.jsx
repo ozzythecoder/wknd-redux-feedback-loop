322629cee53d823cb66d1bee3f2d7af3021f3c03
August McAllister <august.chm@gmail.com>
2022-12-03 12:29:00 -0600

    Admin & body styling

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import './Admin.css'
+
 export default function Admin() {
   
   useEffect(() => { getFeedback() }, [])
@@ -17,14 +19,20 @@ export default function Admin() {
   }
   
   const feedbackTable = feedbackData.map((item, index) => {
-    
+
     return (
       <tr key={index}>
-        <td>{item.feeling}</td>
-        <td>{item.understanding}</td>
-        <td>{item.support}</td>
-        <td>{item.comments}</td>
-        <td>{item.date}</td>
+        <td
+          className={item.feeling < 3 ? 'red-bg' : ''} >
+          {item.feeling}</td>
+        <td
+          className={item.understanding < 3 ? 'red-bg' : ''} >
+          {item.understanding}</td>
+        <td
+          className={item.support < 3 ? 'red-bg' : ''}>
+          {item.support}</td>
+        <td className="comments-cell">{item.comments}</td>
+        <td className="date-cell">{new Date(item.date).toLocaleDateString()}</td>
       </tr>
     )
   })
