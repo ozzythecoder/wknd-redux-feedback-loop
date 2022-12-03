3b0d9cabf46f2aa5546bb7f73d1bc8b5b90285cd
August McAllister <august.chm@gmail.com>
2022-12-03 12:45:00 -0600

    Added landing page for submission success

--- src/components/SubmitSuccess/SubmitSuccess.jsx
+++ src/components/SubmitSuccess/SubmitSuccess.jsx
@@ -2,9 +2,13 @@ import { useHistory } from "react-router-dom"
 
 export default function SubmitSuccess() {
 
+  const history = useHistory();
+
   return (
     <div>
-
+      <h2>Success!</h2>
+      <p>Thank you for submitting your feedback!</p>
+      <button onClick={history.push('/')}>RETURN HOME</button>
     </div>
   )
 }
