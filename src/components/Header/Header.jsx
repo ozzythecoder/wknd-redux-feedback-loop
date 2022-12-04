1c12de2f0d131ecda349288f34b3a045804c45ba
August McAllister <august.chm@gmail.com>
2022-12-03 20:43:08 -0600

    Added link to home page in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
-export default function Header() {
+import { useHistory } from "react-router-dom"
 
+export default function Header() {
+  const history = useHistory();
 
   return (
     <header className='App-header'>
       <h1 className='App-title'>
-        Feedback!</h1>
+        <a href="/#/">
+          Feedback!
+        </a>
+      </h1>
       <h4>Don't forget it!</h4>
     </header>
   )
